0014afeaa4d68783850191d103ab9176d21949b7
Neil Larion <neil.larion@gmail.com>
2025-04-07 17:34:00 -0700

    retrain model with tensorflow 2.15.0 tfjs fixing issue at runtime.

--- chromeExtension/background.js
+++ chromeExtension/background.js
@@ -24,3 +24,42 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 
     return true; // Required for async sendResponse
 });
+
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//     var elmArr = request.message;
+//     console.log("received array: "+elmArr.length);
+//     var running = false;
+//     var i = 0;
+
+//         async function tokenize(message) {
+//             return makeTokens(message);
+//         }
+
+//         async function looprun(elmArr){
+//             await wasm_bindgen('without_a_bundler_bg.wasm');
+//             var startTime = new Date();
+//             var tokenArr = [];
+//             // todo latest idea is to try and convert wasm to use array and spit back array of arrays.
+
+//             // for (let i = 0; i < elmArr.length; i++) {
+//             //     var tokens = await tokenize(elmArr[i]);
+//             //     tokenArr.push(tokens);
+//             // }
+//             // both work about the same
+//             var tokens = await tokenize(elmArr);
+
+//             console.log(tokens);
+// 		    console.log("seconds to build tokjen array: ",(new Date() - startTime) / 1000);
+
+//             var results = await predictFromToken(tokens);
+//             return results;
+//         }
+            
+//         looprun(elmArr).then(x=>{
+//             sendResponse(x);
+//         });
+//         return true;
+//         //return Promise.resolve({response: "stopped"});
+        
+// });
+
